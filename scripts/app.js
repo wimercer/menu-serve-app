3711fe5e2ce2c55645838e4282f6977fb50ec631
Wayne Mercer <wimercer@outlook.com>
2018-05-11 23:13:18 +0100

    added javascript service worker and main app js

---
+++ scripts/app.js
@@ -0,0 +1,64 @@
+﻿// Copyright 2016 Google Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+(function () {
+    'use strict';
+
+    var app = {
+        isLoading: true
+    };
+
+
+    /*****************************************************************************
+     *
+     * Event listeners for UI elements
+     *
+     ****************************************************************************/
+
+    document.getElementById('testButton').addEventListener('click', function () {
+
+        alert('Yay! ' + app.isLoading);
+
+    });
+
+
+    /*****************************************************************************
+     *
+     * Methods to update/refresh the UI
+     *
+     ****************************************************************************/
+
+   // something
+
+    /*****************************************************************************
+     *
+     * Methods for dealing with the model
+     *
+     ****************************************************************************/
+
+    // something
+    
+    /************************************************************************
+     *
+     * Code required to start the app
+     *
+     ************************************************************************/
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function () { console.log('Service Worker Registered'); });
+    }
+
+})();
---
+++ service-worker.js
@@ -0,0 +1,59 @@
+﻿// Copyright 2016 Google Inc.
+// https://github.com/googlecodelabs/your-first-pwapp/blob/master/final/service-worker.js
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//      http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var dataCacheName = 'menu-serve-data-v1';
+var cacheName = 'menu-serve-PWA-v1';
+
+var filesToCache = [
+
+    // pages
+    '/',
+    '/index.html',
+
+    // scripts
+    //'/scripts/app.js',
+
+    // styles
+
+    // images
+
+];
+
+// Install and cache application files
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// Activate 
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName && key !== dataCacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
